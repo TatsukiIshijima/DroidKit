831d402c48f3cb0ff4a688d71a435e0862694cf0
TatsukiIshijima <tatsuki445@gmail.com>
2023-03-21 17:52:35 +0900

    create DroidBLE

---
+++ DroidKit/src/main/java/com/tatsuki/droidkit/DroidBLE.kt
@@ -0,0 +1,10 @@
+package com.tatsuki.droidkit
+
+object DroidBLE {
+  const val W32_CONTROL_HUB = "w32 ControlHub"
+  const val W32_SERVICE_UUID = "d9d9e9e0-aa4e-4797-8151-cb41cedaf2ad"
+  const val W32_CHARACTERISTIC = "d9d9e9e1-aa4e-4797-8151-cb41cedaf2ad"
+  const val W32_BOARD_CONTROL_CHARACTERISTIC = "d9d9e9e2-aa4e-4797-8151-cb41cedaf2ad"
+  const val W32_AUTO_UPLOAD_CHARACTERISTIC = "d9d9e9e3-aa4e-4797-8151-cb41cedaf2ad"
+  const val CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb"
+}
