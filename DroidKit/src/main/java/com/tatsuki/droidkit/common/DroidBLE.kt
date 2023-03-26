d56f04caf6d54f22df82f97f6006cbee22e3421d
TatsukiIshijima <tatsuki445@gmail.com>
2023-03-26 13:14:36 +0900

    fix typo

--- DroidKit/src/main/java/com/tatsuki/droidkit/common/DroidBLE.kt
+++ DroidKit/src/main/java/com/tatsuki/droidkit/common/DroidBLE.kt
@@ -12,7 +12,7 @@ object DroidBLE {
   const val W32_SERVICE_UUID = "d9d9e9e0-aa4e-4797-8151-cb41cedaf2ad"
   const val W32_CHARACTERISTIC = "d9d9e9e1-aa4e-4797-8151-cb41cedaf2ad"
   const val W32_BOARD_CONTROL_CHARACTERISTIC = "d9d9e9e2-aa4e-4797-8151-cb41cedaf2ad"
-  const val W32_AUTO_UPLOAD_CHARACTERISTIC = "d9d9e9e3-aa4e-4797-8151-cb41cedaf2ad"
+  const val W32_AUDIO_UPLOAD_CHARACTERISTIC = "d9d9e9e3-aa4e-4797-8151-cb41cedaf2ad"
   const val CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb"
 
   const val BLE_NOT_READY_ERROR = -1
