15922aef33ce930a7f61774aace17480f68e5102
TatsukiIshijima <tatsuki445@gmail.com>
2023-04-02 16:47:39 +0900

    create viewmodel

---
+++ app/src/main/java/com/tatsuki/droidkitapp/MainViewModel.kt
@@ -0,0 +1,80 @@
+package com.tatsuki.droidkitapp
+
+import android.app.Application
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.tatsuki.droidkit.*
+import com.tatsuki.droidkit.common.DroidBLE
+import com.tatsuki.droidkit.model.DroidCommand
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+import timber.log.Timber
+
+class MainViewModel(
+  application: Application
+) : AndroidViewModel(application) {
+
+  private val bluetoothAdapter: BluetoothAdapter =
+    (application.applicationContext.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager).adapter
+  private val droidScanner: DroidScanner = DroidScannerImpl(bluetoothAdapter)
+  private val droidConnector: DroidConnector = DroidConnectorImpl(application)
+  private val droidOperator: DroidOperator = DroidOperatorImpl(droidConnector)
+
+  fun connect(
+    timeoutMill: Long = 5000L
+  ) {
+    viewModelScope.launch {
+      try {
+        if (!DroidBLE.isReadyBle(getApplication(), bluetoothAdapter)) {
+          throw IllegalStateException()
+        }
+        withTimeout(timeoutMill) {
+          droidScanner.startScan()
+          val droidDevice = droidScanner.device ?: return@withTimeout
+          droidConnector.connect(droidDevice)
+        }
+      } catch (e: TimeoutCancellationException) {
+        Timber.e("Connection timed out after $timeoutMill milliseconds!")
+        throw e
+      } catch (e: CancellationException) {
+        throw e
+      } catch (e: Exception) {
+        droidScanner.stopScan()
+        droidConnector.disconnect()
+        Timber.e(e)
+      }
+    }
+  }
+
+  fun disconnect() {
+    viewModelScope.launch {
+      try {
+        if (!DroidBLE.isReadyBle(getApplication(), bluetoothAdapter)) {
+          throw IllegalStateException()
+        }
+        droidConnector.disconnect()
+      } catch (e: CancellationException) {
+        throw e
+      } catch (e: Exception) {
+        Timber.e(e)
+      }
+    }
+  }
+
+  fun playSound(soundCommend: DroidCommand.PlaySound) {
+    viewModelScope.launch {
+      try {
+        droidOperator.playSound(soundCommend)
+      } catch (e: CancellationException) {
+        throw e
+      } catch (e: Exception) {
+        Timber.d(e)
+      }
+    }
+  }
+}
