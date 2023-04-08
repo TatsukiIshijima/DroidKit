0ca9d94b4abd6fb9a95006eef0043355d4d0a69d
TatsukiIshijima <tatsuki445@gmail.com>
2023-04-08 16:26:15 +0900

    refactor

--- app/src/main/java/com/tatsuki/droidkitapp/ui/compose/view/ConnectionSection.kt
+++ app/src/main/java/com/tatsuki/droidkitapp/ui/compose/view/ConnectionSection.kt
@@ -1,14 +1,9 @@
 package com.tatsuki.droidkitapp.ui.compose.view
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.tatsuki.droidkitapp.R
 
 @Composable
@@ -17,22 +12,18 @@ fun ConnectionSection(
   onClickConnect: () -> Unit,
   onClickDisconnect: () -> Unit,
 ) {
-  Column(
+  SectionFrame(
     modifier = modifier,
-    horizontalAlignment = Alignment.CenterHorizontally,
-  ) {
-    SectionTitle(
-      modifier = Modifier.fillMaxWidth(),
-      textResource = R.string.connection_section_title,
-    )
-    Spacer(modifier = Modifier.size(8.dp))
-    ActionButtonsRow(
-      positiveButtonLabelId = R.string.connect_button_label,
-      negativeButtonLabelId = R.string.disconnect_button_label,
-      onClickPositive = onClickConnect,
-      onClickNegative = onClickDisconnect,
-    )
-  }
+    titleResource = R.string.connection_section_title,
+    body = {
+      ActionButtonsRow(
+        positiveButtonLabelId = R.string.connect_button_label,
+        negativeButtonLabelId = R.string.disconnect_button_label,
+        onClickPositive = onClickConnect,
+        onClickNegative = onClickDisconnect,
+      )
+    }
+  )
 }
 
 @Preview
